f850d416404b960c9483bd2be562ccc437129ccf
Diego Urbina <diego.urbina1024@gmail.com>
2025-03-02 22:46:45 -0600

    test: add tests for email and username uniqueness validation during user creation

--- src/__tests__/createUserUseCase.test.ts
+++ src/__tests__/createUserUseCase.test.ts
@@ -2,8 +2,10 @@ import { describe, expect, test, vi, beforeEach } from "vitest";
 import UserDto from "../application/dtos/UserDto";
 import IUserRepository from "../application/repositories/IUserRepository";
 import User from "../domain/user/User";
-import { success } from "../shared/result/Result";
+import { Failure, success } from "../shared/result/Result";
 import CreateUserUseCase from "../application/use-cases/user/CreateUserUseCase";
+import EmailAlreadyInUseError from "../application/errors/EmailAlreadyInUseError";
+import UsernameAlreadyInUseError from "../application/errors/UsernameAlreadyInUseError";
 
 describe("CreateUserUseCase", () => {
   let mockUserRepository: IUserRepository;
@@ -43,7 +45,7 @@ describe("CreateUserUseCase", () => {
       lastName: "D",
       username: "j",
       email: "johndoe@spidermail",
-      password: "invld",
+      password: "invalidpassword",
     };
 
     mockUserRepository.add = vi.fn().mockImplementation((user: User) => {
@@ -52,5 +54,47 @@ describe("CreateUserUseCase", () => {
     const result = await createUserUseCase.execute(mockRequestData);
     console.log(JSON.stringify(result, null, 2));
     expect(result.isSuccess).toBe(false);
+  });
+
+  test("should fail to create a user as the email is already in use", async () => {
+    const mockRequestData: UserDto = {
+      firstName: "John",
+      lastName: "Doe",
+      username: "johndoe",
+      email: "johndoe@spidermail.com",
+      password: "StrongAndComplicatedPassword123!",
+    };
+    mockUserRepository.findByEmail = vi
+      .fn()
+      .mockImplementation((user: User) => {
+        return user;
+      });
+    const result = await createUserUseCase.execute(mockRequestData);
+    expect(result.isSuccess).toBe(false);
+    const resultError = result as Failure<EmailAlreadyInUseError>;
+    expect(resultError.error.message).toBe(
+      "Cannot create a user with an email already in use, please provide a valid email address."
+    );
+  });
+
+  test("should fail to create a user as the username provided is already in use", async () => {
+    const mockRequestData: UserDto = {
+      firstName: "John",
+      lastName: "Doe",
+      username: "johndoe",
+      email: "johndoe@spidermail.com",
+      password: "StrongAndComplicatedPassword123!",
+    };
+    mockUserRepository.findByUsername = vi
+      .fn()
+      .mockImplementation((user: User) => {
+        return user;
+      });
+    const result = await createUserUseCase.execute(mockRequestData);
+    expect(result.isSuccess).toBe(false);
+    const resultError = result as Failure<UsernameAlreadyInUseError>;
+    expect(resultError.error.message).toBe(
+      "Cannot create a user with a username already in use, please provide a valid username."
+    );
   });
 });
